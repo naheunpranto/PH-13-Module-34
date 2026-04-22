0cf85693d5bdd9b2a1df8a028317a38b5bce86ae
Naheun Alam <naheunpranto258@gmail.com>
2026-04-22 18:10:44 +0600

    use daisy UI pricing card to modify for the component

--- src/components/PircingOptions/PricingOptions.jsx
+++ src/components/PircingOptions/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCard from '../PricingCard/PricingCard';
+import DaisyPricingCard from '../DaisyPricingCard/DaisyPricingCard';
 
 const PricingOptions = ({pricingPromise}) => {
 
@@ -9,14 +10,21 @@ const PricingOptions = ({pricingPromise}) => {
     return (
         <div>
             <h2 className='text-5xl text-center mt-10 mb-10'>Get our Membership</h2>
-            <div className='grid md:grid-cols-3 gap-8'>
-                {
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+                {/* {
                     pricingData.map(pricing => <PricingCard 
                         key={pricing.id}
                         pricing = {pricing}>
                         </PricingCard>)
+                } */}
+                {
+                    pricingData.map(pricing => <DaisyPricingCard
+                    key = {pricing.id}
+                    pricing = {pricing}
+                    ></DaisyPricingCard>)
                 }
             </div>
+            
         </div>
     );
 };
--- src/components/PricingCard/PricingCard.jsx
+++ src/components/PricingCard/PricingCard.jsx
@@ -8,14 +8,14 @@ const PricingCard = ({pricing}) => {
     console.log(pricing);
 
     return (
-        <div className='border bg-amber-600 rounded-2xl p-4'>
+        <div className='flex flex-col border bg-amber-600 rounded-2xl p-4'>
             {/* card header */}
             <div>
                 <h1 className="text-7xl">{plan}</h1>
                 <h4 className="text-3xl">{price}</h4>
             </div>
             {/* card body */}
-            <div className='bg-amber-400 p-4 rounded-2xl mt-10'>
+            <div className='bg-amber-400 p-4 rounded-2xl mt-10 flex-1'>
                 <p>{description}</p>
                 {
                     features.map((feature, index) => <PricingFeature
@@ -24,6 +24,7 @@ const PricingCard = ({pricing}) => {
                     </PricingFeature>)
                 }
             </div>
+            <button className="btn w-full mt-4">Subscribe</button>
         </div>
     );
 };
